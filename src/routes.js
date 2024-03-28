468d926228a68181cfcfa2f9f25805af0b1ce276
Antonio-217 <antoniocbuenas@gmail.com>
2024-03-27 22:03:13 -0300

    Desenvolvido as rotas, controller e implementado as migrations.

---
+++ src/routes.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const routes = express.Router();
+
+const PlanetController = require('../controller/PlanetController');
+const Planet = require('../models/Planet');
+
+routes.post('/planets', PlanetController.store);
+routes.get('/planets', PlanetController.index);
+routes.put('/planets/:id', PlanetController.put);
+routes.delete('/planets/:id', PlanetController.delete);
+
+module.exports = routes;
